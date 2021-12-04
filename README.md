dee6a54691225b4af0fae284409ed74d4d337735
Jason Rowland <jsonxr@gmail.com>
2021-12-04 13:56:02 -0600

    Lesson 2

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
     Publisher: Microsoft
     VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp
 - enable use global mono to always
+
+- Configure godot to work with VSCode: https://gist.github.com/paulloz/30ae499c1fc580a2f3ab9ecebe80d9ba
+
+- Hmmm, couldn't get to work with VSCode with proper code completion. Visual Studio for Mac worked to give me code completion but can't open it via godot.
--- scripts/Steve.cs
+++ scripts/Steve.cs
@@ -1,23 +1,48 @@
-using static Godot.GD;
 using Godot;
-using System;
 
-public class Steve : KinematicBody
-{
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		Print("Hello Godot!");
-	}
-	public override void 
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+public class Steve : KinematicBody {
+  // Declare member variables here. Examples:
+  // private int a = 2;
+  // private string b = "text";
+  private int count;
+  readonly private string name = "Jason";
+  const float SPEED = 6;
+
+  private Vector3 velocity = new Vector3(0, 0, 0);
+
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready() {
+    GD.Print($"Hello World 2 {name}");
+  }
+
+  public override void _PhysicsProcess(float delta) {
+    base._PhysicsProcess(delta);
+    this.count++;
+    if (Input.IsActionPressed("ui_right")) {
+      velocity.x = SPEED;
+    } else if (Input.IsActionPressed("ui_left")) {
+      velocity.x = -SPEED;
+    } else {
+      velocity.x = Mathf.Lerp(velocity.x, 0, 0.1f);
+    }
+
+    if (Input.IsActionPressed("ui_up")) {
+      velocity.z = -SPEED;
+    } else if (Input.IsActionPressed("ui_down")) {
+      velocity.z = SPEED;
+    } else {
+      velocity.z = Mathf.Lerp(velocity.z, 0, 0.1f);
+    }
+
+    MoveAndSlide(velocity);
+  }
+
+
+  //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  //  public override void _Process(float delta)
+  //  {
+  //      
+  //  }
 }
